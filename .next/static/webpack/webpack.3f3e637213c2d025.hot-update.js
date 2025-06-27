aec08fed0cfa28f0052814f4fbdfa171bd42d064
Lipo <lipo6942@gmail.com>
2025-06-27 14:43:45 +0000

    chore(ai): Disable Genkit features to fix build error

--- .next/static/webpack/webpack.3f3e637213c2d025.hot-update.js
+++ .next/static/webpack/webpack.3f3e637213c2d025.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "65418b73fc23fd71"; }
+/******/ 	__webpack_require__.h = function() { return "932a82fd988f7abd"; }
 /******/ }();
 /******/ 
 /******/ }
