489c3a6a20bfede1d37cb10674366d2fe708d30c
Lipo <lipo6942@gmail.com>
2025-06-27 18:46:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c7bac2b8a0c82bc1"; }
+/******/ 		__webpack_require__.h = function() { return "16c1f8b46dad1256"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/ai/flows/generate-list-flow.ts
+++ src/ai/flows/generate-list-flow.ts
@@ -8,7 +8,7 @@
  */
 
 import {ai} from '@/ai/genkit';
-import {z} from 'genkit/zod';
+import {z} from 'zod';
 
 const categories = [
   'Fruits et Légumes',
