8bab3004465931a0a80d8e89137041d3b4a62732
Lipo <lipo6942@gmail.com>
2025-06-27 13:27:29 +0000

    comment envoyer ces changements sur github?

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("af89146b7cd80828")
+/******/ 		__webpack_require__.h = () => ("dd4701852bfd573d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/next.config.ts
@@ -0,0 +1,24 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('handlebars');
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
