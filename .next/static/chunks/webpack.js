623251b62a7f486bc2e29cfe882cfc9f28a70842
Lipo <lipo6942@gmail.com>
2025-06-28 19:10:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05481c6951df3d04"; }
+/******/ 		__webpack_require__.h = function() { return "5e64a273af5c441c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
