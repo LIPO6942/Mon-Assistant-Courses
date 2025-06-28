03b7ea7611864bd7191e17095c8e6edd58c49fd1
Lipo <lipo6942@gmail.com>
2025-06-28 09:25:06 +0000

    ca na'a pas marché , ici aussi le module ia ne fonctionne pas mais avec

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("a04614cab00f5457")
+/******/ 		__webpack_require__.h = () => ("53b309eda4c54c12")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
