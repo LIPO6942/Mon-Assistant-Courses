71610128ce05fd7b34136d56067c2e9f5a4a6a2d
Lipo <lipo6942@gmail.com>
2025-06-29 23:49:03 +0000

    rendre l'application plus attrayante , agreable , permettre d'ajouter l'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f78033b5add23ed0"; }
+/******/ 		__webpack_require__.h = function() { return "e0ed8fe439587cef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
