96e381df466257f4564da0b922452183987e6767
Lipo <lipo6942@gmail.com>
2025-06-30 19:23:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1d3a04f8fd559994"; }
+/******/ 		__webpack_require__.h = function() { return "6882cb0cb62574b0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.af4ba5a8f209f553.hot-update.js
+++ .next/static/webpack/webpack.af4ba5a8f209f553.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1d3a04f8fd559994"; }
+/******/ 	__webpack_require__.h = function() { return "50c272e7bc602299"; }
 /******/ }();
 /******/ 
 /******/ }
