f90d60f5e7896d1c78f84fe0497eaf9c0edc09dc
Lipo <lipo6942@gmail.com>
2025-07-02 00:42:12 +0000

    Erreur NOT_FOUND: Model 'gemini-pro' not found (utilise gemini 2.5 flash

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83d9bd4e55c0450e"; }
+/******/ 		__webpack_require__.h = function() { return "eb9fd34d1c4403c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
