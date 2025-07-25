e7ec4c29032f2aedadefc8e32645e974bf646608
Lipo <lipo6942@gmail.com>
2025-07-25 14:30:53 +0000

    Réduire la taille de la police "total a payer" et "total acheté "  pour

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "555a2866a0a4a898"; }
+/******/ 		__webpack_require__.h = function() { return "143af501677e24cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
