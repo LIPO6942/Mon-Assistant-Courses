f991ddf83223435cd15f5d5c604a0d08ac5c7c03
Lipo <lipo6942@gmail.com>
2025-08-10 23:40:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button size="lg">

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "365f8645dd8cbef6"; }
+/******/ 		__webpack_require__.h = function() { return "225e0f1de2b2b762"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
